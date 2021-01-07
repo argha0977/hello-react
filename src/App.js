c2135e6aea2d28b44e892d563fce1e7f5fe6d253
Argha <email2argha@gmail.com>
2021-01-07 19:02:16 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+
+import { withAuthenticator } from '@aws-amplify/ui-react'
+Amplify.configure(awsExports);
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Hello React
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        </header>
+      </div>
+    );
+  }
+  
 }
 
-export default App;
+//export default App;
+export default withAuthenticator(App);
